7177879dac24c3eb6ec86f2a4eaa852684cdccbb
Zach Gardner <zgardner@keyholesoftware.com>
2016-06-05 19:45:38 -0500

    Switching the class path to match the repository name

--- src/main/java/com/zgardner/microservice/platform/configurationService/Application.java
+++ src/main/java/com/zgardner/microservice/platform/configurationService/Application.java
@@ -1,4 +1,4 @@
-package com.zgardner.configurationService;
+package com.zgardner.microservice.platform.configurationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
