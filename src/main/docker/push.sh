b5888fb7bfcbce43230b6a8d5d9fab661a3760cd
Zach Gardner <z.gardner@hotmail.com>
2016-07-24 21:24:10 -0500

    Removing the old non-linux versions.

--- src/main/docker/push.sh
+++
@@ -1,2 +0,0 @@
-docker tag -f microservice/configurationservice:latest 192.168.159.1:7000/microservice/configurationservice:latest
-docker push 192.168.159.1:7000/microservice/configurationservice:latest
